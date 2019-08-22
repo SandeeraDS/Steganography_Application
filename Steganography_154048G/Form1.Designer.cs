03cb99b2c82a8166f8263cf38c49f7fbbfd19bb6
sandeeraDS <sandeerads@gmail.com>
2019-08-22 18:01:02 +0530

    finished

--- Steganography_154048G/Form1.Designer.cs
+++ Steganography_154048G/Form1.Designer.cs
@@ -28,242 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.hideButton = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.dataTextBox = new System.Windows.Forms.TextBox();
             this.extractButton = new System.Windows.Forms.Button();
+            this.hideButton = new System.Windows.Forms.Button();
             this.imagePictureBox = new System.Windows.Forms.PictureBox();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.imageToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.textToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.imageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.textToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.saveButton = new System.Windows.Forms.Button();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).BeginInit();
-            this.menuStrip1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // hideButton
+            // groupBox4
             // 
-            this.hideButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.hideButton.Location = new System.Drawing.Point(7, 353);
-            this.hideButton.Margin = new System.Windows.Forms.Padding(4);
-            this.hideButton.Name = "hideButton";
-            this.hideButton.Size = new System.Drawing.Size(241, 57);
-            this.hideButton.TabIndex = 0;
-            this.hideButton.Text = "Hide Text";
-            this.hideButton.UseVisualStyleBackColor = true;
+            this.groupBox4.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox4.Location = new System.Drawing.Point(7, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(504, 334);
+            this.groupBox4.TabIndex = 12;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Image";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox3.Controls.Add(this.dataTextBox);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(520, 78);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(471, 268);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Enter the Message";
             // 
             // dataTextBox
             // 
             this.dataTextBox.Font = new System.Drawing.Font("Tahoma", 12F);
             this.dataTextBox.Location = new System.Drawing.Point(7, 22);
             this.dataTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.dataTextBox.MaxLength = 300;
             this.dataTextBox.Multiline = true;
             this.dataTextBox.Name = "dataTextBox";
             this.dataTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.dataTextBox.Size = new System.Drawing.Size(461, 302);
+            this.dataTextBox.Size = new System.Drawing.Size(461, 226);
             this.dataTextBox.TabIndex = 2;
             this.dataTextBox.Tag = "";
             // 
             // extractButton
             // 
+            this.extractButton.BackColor = System.Drawing.Color.LightSlateGray;
+            this.extractButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.extractButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.extractButton.Location = new System.Drawing.Point(724, 353);
+            this.extractButton.Location = new System.Drawing.Point(724, 362);
             this.extractButton.Margin = new System.Windows.Forms.Padding(4);
             this.extractButton.Name = "extractButton";
             this.extractButton.Size = new System.Drawing.Size(241, 57);
             this.extractButton.TabIndex = 3;
-            this.extractButton.Text = "Extract Text";
-            this.extractButton.UseVisualStyleBackColor = true;
+            this.extractButton.Text = "Extract Message";
+            this.extractButton.UseVisualStyleBackColor = false;
+            this.extractButton.Click += new System.EventHandler(this.extractButton_Click);
+            // 
+            // hideButton
+            // 
+            this.hideButton.BackColor = System.Drawing.Color.LightSlateGray;
+            this.hideButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.hideButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hideButton.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.hideButton.Location = new System.Drawing.Point(9, 362);
+            this.hideButton.Margin = new System.Windows.Forms.Padding(4);
+            this.hideButton.Name = "hideButton";
+            this.hideButton.Size = new System.Drawing.Size(241, 57);
+            this.hideButton.TabIndex = 0;
+            this.hideButton.Text = "Hide Message";
+            this.hideButton.UseVisualStyleBackColor = false;
+            this.hideButton.Click += new System.EventHandler(this.hideButton_Click);
             // 
             // imagePictureBox
             // 
+            this.imagePictureBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.imagePictureBox.Image = global::Steganography_154048G.Properties.Resources.click_to_upload;
             this.imagePictureBox.Location = new System.Drawing.Point(9, 34);
             this.imagePictureBox.Margin = new System.Windows.Forms.Padding(4);
             this.imagePictureBox.Name = "imagePictureBox";
-            this.imagePictureBox.Size = new System.Drawing.Size(502, 302);
+            this.imagePictureBox.Size = new System.Drawing.Size(495, 302);
             this.imagePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.imagePictureBox.TabIndex = 4;
             this.imagePictureBox.TabStop = false;
+            this.imagePictureBox.Click += new System.EventHandler(this.imagePictureBox_Click);
             // 
-            // menuStrip1
+            // saveButton
             // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem,
-            this.helpToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(1021, 28);
-            this.menuStrip1.TabIndex = 5;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // fileToolStripMenuItem
-            // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openToolStripMenuItem,
-            this.saveToolStripMenuItem});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
-            this.fileToolStripMenuItem.Text = "File";
-            // 
-            // openToolStripMenuItem
-            // 
-            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.imageToolStripMenuItem1,
-            this.textToolStripMenuItem1});
-            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
-            this.openToolStripMenuItem.Text = "Open";
-            // 
-            // imageToolStripMenuItem1
-            // 
-            this.imageToolStripMenuItem1.Name = "imageToolStripMenuItem1";
-            this.imageToolStripMenuItem1.Size = new System.Drawing.Size(126, 26);
-            this.imageToolStripMenuItem1.Text = "Image";
-            // 
-            // textToolStripMenuItem1
-            // 
-            this.textToolStripMenuItem1.Name = "textToolStripMenuItem1";
-            this.textToolStripMenuItem1.Size = new System.Drawing.Size(126, 26);
-            this.textToolStripMenuItem1.Text = "Text";
-            // 
-            // saveToolStripMenuItem
-            // 
-            this.saveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.imageToolStripMenuItem,
-            this.textToolStripMenuItem});
-            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
-            this.saveToolStripMenuItem.Text = "Save";
-            // 
-            // imageToolStripMenuItem
-            // 
-            this.imageToolStripMenuItem.Name = "imageToolStripMenuItem";
-            this.imageToolStripMenuItem.Size = new System.Drawing.Size(126, 26);
-            this.imageToolStripMenuItem.Text = "Image";
-            // 
-            // textToolStripMenuItem
-            // 
-            this.textToolStripMenuItem.Name = "textToolStripMenuItem";
-            this.textToolStripMenuItem.Size = new System.Drawing.Size(126, 26);
-            this.textToolStripMenuItem.Text = "Text";
+            this.saveButton.BackColor = System.Drawing.Color.LightSlateGray;
+            this.saveButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(392, 362);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(4);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(217, 57);
+            this.saveButton.TabIndex = 10;
+            this.saveButton.Text = "Save Image";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Controls.Add(this.saveButton);
             this.groupBox2.Controls.Add(this.imagePictureBox);
             this.groupBox2.Controls.Add(this.hideButton);
             this.groupBox2.Controls.Add(this.extractButton);
             this.groupBox2.Controls.Add(this.groupBox3);
             this.groupBox2.Controls.Add(this.groupBox4);
-            this.groupBox2.Location = new System.Drawing.Point(12, 31);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(988, 655);
+            this.groupBox2.Size = new System.Drawing.Size(997, 441);
             this.groupBox2.TabIndex = 10;
             this.groupBox2.TabStop = false;
             // 
-            // saveButton
+            // label1
             // 
-            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.saveButton.Location = new System.Drawing.Point(389, 353);
-            this.saveButton.Margin = new System.Windows.Forms.Padding(4);
-            this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(217, 57);
-            this.saveButton.TabIndex = 10;
-            this.saveButton.Text = "Save Image";
-            this.saveButton.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(524, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Image Link";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // groupBox3
+            // textBox1
             // 
-            this.groupBox3.BackColor = System.Drawing.Color.Transparent;
-            this.groupBox3.Controls.Add(this.dataTextBox);
-            this.groupBox3.Location = new System.Drawing.Point(517, 11);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(471, 335);
-            this.groupBox3.TabIndex = 11;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Enter Text Here";
-            // 
-            // groupBox4
-            // 
-            this.groupBox4.BackColor = System.Drawing.Color.Transparent;
-            this.groupBox4.Location = new System.Drawing.Point(7, 12);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(504, 334);
-            this.groupBox4.TabIndex = 12;
-            this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "Add Image Here";
-            // 
-            // aboutToolStripMenuItem
-            // 
-            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
-            this.aboutToolStripMenuItem.Text = "About";
-            // 
-            // helpToolStripMenuItem
-            // 
-            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.aboutToolStripMenuItem});
-            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
-            this.helpToolStripMenuItem.Text = "Help";
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(646, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(319, 51);
+            this.textBox1.TabIndex = 14;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(1021, 465);
-            this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.groupBox2);
-            this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "154048G-Steganography";
-            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).EndInit();
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button hideButton;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.TextBox dataTextBox;
         private System.Windows.Forms.Button extractButton;
+        private System.Windows.Forms.Button hideButton;
         private System.Windows.Forms.PictureBox imagePictureBox;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem imageToolStripMenuItem1;
-        private System.Windows.Forms.ToolStripMenuItem textToolStripMenuItem1;
-        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem imageToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem textToolStripMenuItem;
-        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button saveButton;
-        private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.GroupBox groupBox4;
-        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Steganography_154048G/Form1.cs
+++ Steganography_154048G/Form1.cs
@@ -1,20 +1,147 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
 
 namespace Steganography_154048G
 {
     public partial class Form1 : Form
     {
+        private Bitmap bmp = null;
+        private string extractedText = string.Empty;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void imagePictureBox_Click(object sender, EventArgs e)
+        {
+            dataTextBox.Text = "";
+            textBox1.Text = "";
+            OpenFileDialog open_dialog = new OpenFileDialog();
+            open_dialog.Filter = "Image Files (*.jpeg; *.png; *.bmp)|*.jpg; *.png; *.bmp";
+
+            if (open_dialog.ShowDialog() == DialogResult.OK)
+            {
+                imagePictureBox.Image = Image.FromFile(open_dialog.FileName);
+                textBox1.Text = open_dialog.FileName;
+            }
+        }
+
+        private void hideButton_Click(object sender, EventArgs e)
+        {
+            bmp = (Bitmap)imagePictureBox.Image;
+
+            
+
+            if (bmp == null)
+            {
+                MessageBox.Show("No Image file", "Error");
+
+                return;
+            }
+
+            int h = bmp.Height;
+            int w = bmp.Width;
+            int pixels = h * w;
+
+            
+            string text = dataTextBox.Text;
+
+            if (pixels * 3 <(text.Length*8)+8) {
+
+                MessageBox.Show("Image is not enough", "Error");
+
+                return;
+            }
+
+                if (text.Equals(""))
+            {
+                MessageBox.Show("The text you want to hide can't be empty", "Error");
+
+                return;
+            }
+
+            if (text.Length > 255)
+            {
+                MessageBox.Show("The text is too big !!! Maximum length is 255", "Error");
+
+                return;
+            }
+
+            for(int i = 0; i < text.Length; i++)
+            {
+                if (text[i] < 32 || text[i] > 125)
+                {
+
+                    MessageBox.Show("Message contain invalid character(s)", "Error");
+                    return;
+                }
+            }
+
+
+            bmp = SteganographyOps.embedText(text, bmp);
+
+            MessageBox.Show("Your text was hidden in the image successfully!", "OK");
+            dataTextBox.Text = "";
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save_dialog = new SaveFileDialog();
+            save_dialog.Filter = "Png Image|*.png|Bitmap Image|*.bmp";
+
+            if (save_dialog.ShowDialog() == DialogResult.OK)
+            {
+                switch (save_dialog.FilterIndex)
+                {
+                    case 0:
+                        {
+                            bmp.Save(save_dialog.FileName, ImageFormat.Png);
+                           
+                        }
+                        break;
+                    case 1:
+                        {
+                            bmp.Save(save_dialog.FileName, ImageFormat.Bmp);
+                        }
+                        break;
+                }
+
+                textBox1.Text = save_dialog.FileName;
+            }
+           
+        }
+
+        private void extractButton_Click(object sender, EventArgs e)
+        {
+            bmp = (Bitmap)imagePictureBox.Image;
+
+            string extractedText = SteganographyOps.extractText(bmp);
+
+            if (extractedText==null)
+            {
+                MessageBox.Show("Not contain a secret message", "Error");
+            }
+            else
+            {
+                dataTextBox.Text = extractedText;
+            }
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
